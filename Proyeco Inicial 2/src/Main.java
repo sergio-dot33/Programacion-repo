06eb6822f27d31ec16b1c4f8f44d64c9e87c70e9
sergio-dot33 <sergio.dot11@gmail.com>
2025-10-09 11:27:26 +0200

    update test

---
+++ Proyeco Inicial 2/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main (String[] args) {
+        System.out.println("Probando Java test");
+        System.out.println("Probando probando test2");
+    }
+
+    //comentario de pruebas
+
+    /*
+    ddddd
+    dddd
+     */
+
+
+}
